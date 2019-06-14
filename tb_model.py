de3115b438b432395ed84f221ab1d43c0a92e5c1
James Trauer <james.trauer@monash.edu>
2019-06-14 14:15:01 +1000

    get tb_model running with age-specific latency parameters
    Checked that the behaviour is similar to that coming through in the R-based version of the code.

--- tb_model.py
+++ tb_model.py
@@ -18,6 +18,9 @@ def add_vtp_latency_parameters(parameters_, change_time_unit=365.25):
 
 
 def get_age_specific_latency_parameters(parameter, change_time_unit=365.25):
+    """
+    get the age-specific latency parameters estimated by Ragonnet et al
+    """
     age_stratified_parameters = \
         {"early_progression":
              {"0": 6.6e-3,
@@ -31,8 +34,16 @@ def get_age_specific_latency_parameters(parameter, change_time_unit=365.25):
              {"0": 1.9e-11,
               "5": 6.4e-6,
               "15": 3.3e-6}}
-    result = {key: value * change_time_unit for key, value in age_stratified_parameters[parameter].items()}
-    return result
+    return {"adjustments":
+                {key: value * change_time_unit for key, value in age_stratified_parameters[parameter].items()},
+            "overwrite": [0, 5, 15]}
+
+
+def get_all_age_specific_latency_parameters(parameters_=("early_progression", "stabilisation", "late_progression")):
+    """
+    collate all the latency parameters together from the previous function
+    """
+    return {parameter: get_age_specific_latency_parameters(parameter) for parameter in parameters_}
 
 
 def add_standard_latency_flows(flows_):
@@ -52,12 +63,13 @@ if __name__ == "__main__":
     case_fatality_rate = 0.4
     untreated_disease_duration = 3.0
     parameters = \
-        {"beta": 50.0,
+        {"beta": 10.0,
          "recovery": case_fatality_rate / untreated_disease_duration,
          "infect_death": (1.0 - case_fatality_rate) / untreated_disease_duration,
          "universal_death_rate": 1.0 / 50.0}
     parameters = add_vtp_latency_parameters(parameters)
-    times = numpy.linspace(0.0, 200.0, 200).tolist()
+
+    times = numpy.linspace(0.0, 200.0, 201).tolist()
     flows = [{"type": "infection_frequency", "parameter": "beta", "origin": "susceptible", "to": "early_latent"},
              {"type": "infection_frequency", "parameter": "beta", "origin": "recovered", "to": "early_latent"},
              {"type": "standard_flows", "parameter": "recovery", "origin": "infectious", "to": "recovered"},
@@ -68,16 +80,26 @@ if __name__ == "__main__":
     tb_model = summer_model.StratifiedModel(
         times, ["susceptible", "early_latent", "late_latent", "infectious", "recovered"], {"infectious": 1e-3},
         parameters, flows, birth_approach="replace_deaths")
+
+    something = get_all_age_specific_latency_parameters()
+    print(something)
+
+    # # print(flows)
     tb_model.stratify("age", [5, 15], [],
-                      adjustment_requests=
-                      {"early_progression": {"adjustments": get_age_specific_latency_parameters("early_progression")},
-                       "stabilisation": {"adjustments": get_age_specific_latency_parameters("stabilisation")},
-                       "late_progression": {"adjustments": get_age_specific_latency_parameters("late_progression")}},
-                      report=True)
+                      adjustment_requests=get_all_age_specific_latency_parameters(),
+                      report=False)
     tb_model.run_model()
 
     # get outputs
-    matplotlib.pyplot.plot(times, tb_model.outputs[:, tb_model.compartment_names.index("infectiousXage_15")] * 1e5)
+    infectious_population = tb_model.outputs[:, tb_model.compartment_names.index("infectiousXage_0")] + \
+                            tb_model.outputs[:, tb_model.compartment_names.index("infectiousXage_5")] + \
+                            tb_model.outputs[:, tb_model.compartment_names.index("infectiousXage_15")]
+
+
+    matplotlib.pyplot.plot(times, infectious_population * 1e5)
+
+    # tb_model.death_flows.to_csv("tb_model_deaths.csv")
+
     # matplotlib.pyplot.xlim((1e3, 2e3))
     # matplotlib.pyplot.ylim((0.0, 100.0))
     matplotlib.pyplot.show()
